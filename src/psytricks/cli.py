ec9c50bda070f583fba84fb09723b70d8f6ddf46
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-03-24 21:43:04 +0100

    Make config parameter mandatory

--- src/psytricks/cli.py
+++ src/psytricks/cli.py
@@ -26,7 +26,7 @@ def configure_logging(verbose: int):
 
 
 @click.command(help="Run the PSyTricks command line interface.")
-@click.option("--config", type=str, help="A JSON configuration file.")
+@click.option("--config", type=str, help="A JSON configuration file.", required=True)
 @click.option(
     "-v",
     "--verbose",
