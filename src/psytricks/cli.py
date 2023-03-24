146f351a8d4b34da45629ee9d72b9771aa1f26fb
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-03-24 17:57:00 +0100

    Migrate to a src layout

---
+++ src/psytricks/cli.py
@@ -0,0 +1,37 @@
+import click
+from loguru import logger as log
+
+
+def configure_logging(verbose: int):
+    """Configure loguru logging / change log level.
+
+    Parameters
+    ----------
+    verbose : int
+        The desired log level, 0=WARNING (do not change the logger config),
+        1=INFO, 2=DEBUG, 3=TRACE. Higher values will map to TRACE.
+    """
+    level = "WARNING"
+    if verbose == 1:
+        level = "INFO"
+    elif verbose == 2:
+        level = "DEBUG"
+    elif verbose >= 3:
+        level = "TRACE"
+    # set up logging, loguru requires us to remove the default handler and
+    # re-add a new one with the desired log-level:
+    log.remove()
+    log.add(sys.stderr, level=level)
+    log.info(f"Set logging level to [{level}] ({verbose}).")
+
+
+@click.command(help="Run the PSyTricks command line interface.")
+@click.option("--config", type=str, help="A JSON configuration file.")
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    help="Increase logging verbosity, may be repeated up to 3 times.",
+)
+def run_cli(config, verbose):
+    configure_logging(verbose)
