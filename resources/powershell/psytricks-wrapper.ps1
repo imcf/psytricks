9758c0ac21446bed2de08d707d080670e57c21a0
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-03-24 16:19:08 +0100

    Make PowerShell wrapper a bit more sophisticated
    This is still heavily WIP but it allows for specifying a configuration
    file containing information about where to find the Citrix
    infrastructure and requesting different commands (currently implementing
    "GetMachineStatus" as the first one).

--- resources/powershell/psytricks-wrapper.ps1
+++ resources/powershell/psytricks-wrapper.ps1
@@ -1,6 +1,52 @@
-Get-ChildItem -Path "C:\Temp" | `
-    Select-Object -Property Name,Length,CreationTime | `
-    ConvertTo-Json -Compress
+[CmdletBinding()]
+param (
+    # the JSON config file to use
+    [Parameter(Mandatory = $true)]
+    [string]
+    $JsonConfig,
+
+    [Parameter()]
+    [string]
+    $CommandName = ""
+)
+
+
+try {
+    $Config = Get-Content $JsonConfig | ConvertFrom-Json -EA Stop
+}
+catch {
+    throw "Error reading JSON configuration file: [$JsonConfig]"
+}
+
+Add-PSSnapin Citrix.Broker.Admin.V2
+
+
+if ($CommandName -eq "GetMachineStatus") {
+    $Properties = @(
+        "HostedMachineName",
+        "PowerState",
+        "InMaintenanceMode",
+        "SummaryState",
+        "RegistrationState",
+        "Registered",
+        "SessionUserName",
+        "AssociatedUserUPNs",
+        "AgentVersion",
+        "SessionClientVersion",
+        "SessionDeviceId",
+        "SessionStartTime",
+        "SessionStateChangeTime"
+    )
+    $Data = Get-BrokerMachine -AdminAddress $Config.CitrixDC | `
+        Select-Object -Property $Properties
+
+}
+else {
+    $Data = Get-ChildItem -Path "C:\Temp" | `
+        Select-Object -Property Name, Length, CreationTime
+}
+
+$Data | ConvertTo-Json -Compress
 
 # $json = $("asdf ä ö ü fdsa" | ConvertTo-Json -Compress)
 # $json
