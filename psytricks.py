2e1ef1e712949dfe792ced0aa2e96d4901247677
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2023-03-24 16:19:36 +0100

    Put wrapper call and output parsing into a function

--- psytricks.py
+++ psytricks.py
@@ -2,10 +2,13 @@
 
 import subprocess
 import json
+import os
+import time
 from os.path import dirname, abspath
 from pathlib import Path
 import logging
-from logging import warning, debug
+from logging import warning, info, debug
+from typing import Literal
 
 from pprint import pprint
 
@@ -25,10 +28,62 @@ psscripts = Path(abspath(dirname(__file__))) / "resources" / "powershell"
 pswrapper = psscripts / "psytricks-wrapper.ps1"
 warning(f"Using PowerShell script [{pswrapper}].")
 
-completed = subprocess.run(["powershell", "-File", pswrapper], capture_output=True)
-if completed.stderr:
-    raise RuntimeError(f"PowerShell call had issues: {completed.stderr}!")
+# TODO: make this configurable!
+conffile = Path("config.json")
+debug(f"Using configuration file [{conffile}].")
 
-stdout = completed.stdout.decode(encoding="cp850")
-parsed = json.loads(stdout)
-pprint(parsed)
+
+RequestNames = Literal["GetMachineStatus", "Dummy"]
+
+
+def get_citrix_details(request: RequestNames) -> list:
+    """Call the PowerShell wrapper to retrieve information from Citrix.
+
+    Parameters
+    ----------
+    request : RequestNames
+        The name of the request.
+    """
+    try:
+        tstart = time.time()
+        command = [
+            ps_exe,
+            "-NonInteractive",
+            "-NoProfile",
+            "-File",
+            pswrapper,
+            "-JsonConfig",
+            conffile,
+            "-CommandName",
+            request,
+        ]
+        debug(f"Command for subprocess call: {command}")
+        completed = subprocess.run(
+            command,
+            capture_output=True,
+            check=True,
+        )
+        elapsed = time.time() - tstart
+        debug(f"PowerShell call took {elapsed:.3}s.")
+        if completed.stderr:
+            raise RuntimeError(
+                "Wrapper returned data on STDERR, this is not expected:\n============\n"
+                f"{completed.stderr}\n============\n"
+            )
+    except subprocess.CalledProcessError as ex:
+        raise RuntimeError(
+            f"Call returned a non-zero state: {ex.returncode} {ex.stderr}"
+        ) from ex
+
+    try:
+        stdout = completed.stdout.decode(encoding="cp850")
+        parsed = json.loads(stdout)
+    except Exception as ex:
+        raise RuntimeError("Error decoding / parsing output!") from ex
+
+    return parsed
+
+
+details = get_citrix_details(request="GetMachineStatus")
+info(f"Got details on {len(details)} machines.")
+# pprint(details)
