91038ec65caa6c6cff7aaedec0961389d853ae3a
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2025-04-14 09:57:49 +0200

    Tell ruff the weird pseudo-docstring is alright

--- src/psytricks/__init__.py
+++ src/psytricks/__init__.py
@@ -4,3 +4,7 @@
 """
 
 __version__ = "0.0.0"
+
+# style exceptions for pdoc-include-only docstrings:
+# ruff: noqa: D400  (missing-trailing-period)
+# ruff: noqa: D415  (missing-terminal-punctuation)
